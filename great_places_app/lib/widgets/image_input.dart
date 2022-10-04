73cf1cac81bb16cda7be6ba4836b64d985279126
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2022-10-04 22:40:57 +0500

    13- save place data to local state, show places from local state if present, take picture from camera for places.

--- great_places_app/lib/widgets/image_input.dart
+++ great_places_app/lib/widgets/image_input.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-// import 'package:path/path.dart' as path;
-// import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({Key? key}) : super(key: key);
+  final Function onSelectImage;
+
+  const ImageInput({Key? key, required this.onSelectImage}) : super(key: key);
 
   @override
   ImageInputState createState() => ImageInputState();
@@ -16,18 +18,25 @@ class ImageInputState extends State<ImageInput> {
   File? _storedImage;
 
   Future<void> _takePicture() async {
-    // var _imagePicker = ImagePicker();
+    var imagePicker = ImagePicker();
 
-    // final imageFile = await _imagePicker.pickImage(
-    //   source: ImageSource.camera,
-    //   maxWidth: 600,
-    // );
-    // setState(() {
-    //   _storedImage = File(imageFile!.path);
-    // });
-    // final appDir = await syspaths.getApplicationDocumentsDirectory();
-    // final fileName = path.basename(imageFile!.path);
-    // final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+    final imageFile = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (imageFile == null) {
+      return;
+    }
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage =
+        await File(imageFile.path).copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
   }
 
   @override
@@ -58,7 +67,7 @@ class ImageInputState extends State<ImageInput> {
             icon: const Icon(Icons.camera),
             label: const Text('Take Picture'),
             textColor: Theme.of(context).primaryColor,
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
