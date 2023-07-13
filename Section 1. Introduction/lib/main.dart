bfa95b636596f49cd9fdec556a8df48a750f81af
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2023-07-13 07:40:41 +0500

    S-2: Splitting Code Across Files.

--- Section 1. Introduction/lib/main.dart
+++ Section 1. Introduction/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 main() {
   runApp(
     const MaterialApp(
@@ -8,33 +10,4 @@ main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 16, 2, 41),
-            Color.fromARGB(255, 94, 38, 192),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
