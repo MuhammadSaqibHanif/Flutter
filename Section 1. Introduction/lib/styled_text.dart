845df446b66c443a336a7ad5d9ffe85347a875dd
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2023-07-20 06:54:49 +0500

    S-2: Instance Variables (Properties) & Configurable Widgets.

--- Section 1. Introduction/lib/styled_text.dart
+++ Section 1. Introduction/lib/styled_text.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  // StyledText(String text, {super.key}) : outputText = text;
+  const StyledText(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "Hello World!",
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
