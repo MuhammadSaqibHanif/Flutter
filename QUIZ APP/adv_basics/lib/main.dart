746388469f106a6ba17e0c034afb173314ec24cb
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-01-13 19:58:29 +0500

    S-3: Create start screen, add image, gradient background color, add text, add button.

--- QUIZ APP/adv_basics/lib/main.dart
+++ QUIZ APP/adv_basics/lib/main.dart
@@ -4,9 +4,21 @@ import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 92, 41, 180),
+                Color.fromARGB(255, 52, 5, 134),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
