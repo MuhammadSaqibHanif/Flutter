0b436ec62566cc19761ea4688b6ca57d9f4466e4
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-01-22 02:30:25 +0500

    S-3: Managing The Questions Index As State to go to next one.

--- QUIZ APP/adv_basics/lib/questions_screen.dart
+++ QUIZ APP/adv_basics/lib/questions_screen.dart
@@ -11,9 +11,19 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -30,7 +40,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(onTap: () {}, answerText: answer);
+              return AnswerButton(
+                onTap: answerQuestion,
+                answerText: answer,
+              );
             }),
           ],
         ),
