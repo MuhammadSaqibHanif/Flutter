541d232f75c5b7f7b67978f53f1cb8a6b4f60196
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-01-23 03:36:20 +0500

    S-3: Using Third-Party Packages & Adding Google Fonts and change the questions text styling.

--- QUIZ APP/adv_basics/lib/questions_screen.dart
+++ QUIZ APP/adv_basics/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
@@ -35,7 +36,11 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 121, 121, 233),
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
