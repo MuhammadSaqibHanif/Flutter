06b45d64181ca0e3d455f17d969754df6659ce86
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-01-30 22:02:27 +0500

    S-5: Showing & Managing "Snackbars", undo deleted item and show content if list empty.

--- EXPENSE TRACKER/lib/widgets/expenses.dart
+++ EXPENSE TRACKER/lib/widgets/expenses.dart
@@ -44,13 +44,45 @@ class _ExpnesesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    var expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text(
+        'No expenses found, start adding some.',
+      ),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Expense Tracker"),
@@ -65,10 +97,7 @@ class _ExpnesesState extends State<Expenses> {
         children: [
           const Text("qwer"),
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: mainContent,
           ),
         ],
       ),
