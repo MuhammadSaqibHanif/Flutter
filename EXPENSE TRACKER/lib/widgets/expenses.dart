4542d213149e3e4f94268f28e8bc02af0fae4441
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-01-31 20:15:43 +0500

    S-5: Adding Chart Widgets.

--- EXPENSE TRACKER/lib/widgets/expenses.dart
+++ EXPENSE TRACKER/lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 
@@ -95,7 +96,7 @@ class _ExpnesesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("qwer"),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
