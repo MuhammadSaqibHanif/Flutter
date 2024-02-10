30b701468e263d5a3435b3c3d06662ed6dea3800
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-02-10 15:19:34 +0500

    S-9: Triggering a Notifier Method.

--- meals/lib/providers/favorites_provider.dart
+++ meals/lib/providers/favorites_provider.dart
@@ -5,11 +5,14 @@ import 'package:meals/models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     if (state.contains(meal)) {
       state = state.where((m) => m.id != meal.id).toList();
+
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 }
