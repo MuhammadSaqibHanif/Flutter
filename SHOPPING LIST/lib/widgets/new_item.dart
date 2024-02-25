c6f74969066f4949911a1b42b4d9944b2f54e8ac
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-02-25 17:49:11 +0500

    S-12: Sending a POST Request to the Backend.

--- SHOPPING LIST/lib/widgets/new_item.dart
+++ SHOPPING LIST/lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -24,12 +26,19 @@ class _NewItemState extends State<NewItem> {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https('js-quizzz.firebaseio.com', 'shopping-list.json');
+
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
         ),
       );
     }
