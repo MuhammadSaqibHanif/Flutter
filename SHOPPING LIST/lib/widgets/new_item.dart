cafc694101524e971bf4606c5370a8239e9f8aec
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-02-25 21:42:25 +0500

    S-12: Working with the Request & Waiting for the Response.

--- SHOPPING LIST/lib/widgets/new_item.dart
+++ SHOPPING LIST/lib/widgets/new_item.dart
@@ -22,13 +22,13 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
       final url = Uri.https('js-quizzz.firebaseio.com', 'shopping-list.json');
 
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -41,6 +41,15 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
   }
 
