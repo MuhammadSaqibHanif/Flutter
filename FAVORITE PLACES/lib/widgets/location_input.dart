f530f451e3daf1ccd4166948520748e97fbe3dc5
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-03-18 06:42:33 +0500

    S-13: Displaying a Location Preview Map Snapshot via Google.

--- FAVORITE PLACES/lib/widgets/location_input.dart
+++ FAVORITE PLACES/lib/widgets/location_input.dart
@@ -18,6 +18,17 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyAdnanGhf1IhukOCC06xS5FnzGwCkep3TQ';
+  }
+
   void getCurrentLocation() async {
     Location location = Location();
 
@@ -85,6 +96,15 @@ class _LocationInputState extends State<LocationInput> {
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
+    }
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
     }
 
     return Column(
