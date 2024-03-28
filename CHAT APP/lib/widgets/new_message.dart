2c5bad13978850a52356f7e2f43d333b8c575f86
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2024-03-28 08:05:56 +0500

    S-14: Adding ChatMessages & Input Widgets.

---
+++ CHAT APP/lib/widgets/new_message.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() {
+    final enteredText = _messageController.text;
+
+    if (enteredText.trim().isEmpty) {
+      return;
+    }
+
+    _messageController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              decoration: const InputDecoration(
+                label: Text('Send a message...'),
+              ),
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+            ),
+          ),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ],
+      ),
+    );
+  }
+}
